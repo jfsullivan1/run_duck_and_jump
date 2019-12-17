442017dc831d2de7e15cdc274e09f09d2736824b
Andrew Lefebvre <aml316@my.uri.edu>
2019-12-17 16:11:44 -0500

    Fix merge with sounds

--- src/v2/dino.java
+++ src/v2/dino.java
@@ -281,9 +281,9 @@ public class dino extends PApplet implements ApplicationConstants {
 		imageHeart = loadImage("data/heart.png");
 		imageWood = loadImage("data/wood.png");
 		//Sound files
-		die = new SoundFile(this, "death.wav");
-		jump = new SoundFile(this, "jump.wav");
-		fireball = new SoundFile(this, "fireball.wav");
+		die = new SoundFile(this, "data/death.wav");
+		jump = new SoundFile(this, "data/jump.wav");
+		fireball = new SoundFile(this, "data/fireball.wav");
 		//-----------------------------
 		//	add random amount of ellipses
 		//-----------------------------
@@ -314,6 +314,7 @@ public class dino extends PApplet implements ApplicationConstants {
 	}
 	
 	public void draw() {
+		if(state == 4)new_state = true;
 		if(splashOn == true) {
 			clear();
 			PImage splashBackground = loadImage("data/forest.jpg");
@@ -730,24 +731,6 @@ public class dino extends PApplet implements ApplicationConstants {
 	
 	public void keyPressed() {
 		
-		if(new_state) {
-			switch (keyCode) {
-			case ' '://Jump
-				startTime_ = millis();
-				state = 3;
-				new_state = false;
-				jump.play();
-				interpolate();
-				break;
-			case DOWN:
-				startTime_ = millis();
-				state = 4;
-				new_state = false;
-				interpolate();
-				break;
-			}
-		}
-		
 		switch (key) {
 		case 'n':
 			GraphicObject.setBoundingBoxMode(BoundingBoxMode.NO_BOX);
@@ -765,10 +748,27 @@ public class dino extends PApplet implements ApplicationConstants {
 			drawRefFrame_ = !drawRefFrame_;
 			GraphicObject.setDrafReferenceFrame(drawRefFrame_);
 			break;
-		case 'w':
+		case ' ':
 			if(bullets.size() < 1)
 				bullets.add(new Bullet(XMIN+200, YMAX-525 + movement_v,0,20,20,0,400,0,0));
-				fireball.play();
+			fireball.play();
+			break;
+		case 's':
+			if(new_state) {
+				startTime_ = millis();
+				state = 4;
+				new_state = false;
+				interpolate();
+			}
+			break;
+		case 'w':
+			if(new_state) {
+				startTime_ = millis();
+				state = 3;
+				new_state = false;
+				jump.play();
+				interpolate();
+			}
 			break;
 		
 		}
